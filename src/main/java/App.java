b7bffb915dc5b342c8df36841e2856735ecfacc2
IslamAhmed1092 <eslam1092@hotmail.com>
2022-09-15 17:04:19 +0200

    add some admin ui functionalities and remove unused files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,87 +6,12 @@ import database.PublicAPI;
 import user.UserFactory;
 import ui.UserUI;
 import ui.UserUIFactory;
+import utils.Color;
 
 import java.util.Scanner;
 
 @SuppressWarnings("ALL")
 public class App {
-
-    public static class Colors {
-        // Reset
-        public static final String RESET = "\033[0m";  // Text Reset
-
-        // Regular Colors
-        public static final String BLACK = "\033[0;30m";   // BLACK
-        public static final String RED = "\033[0;31m";     // RED
-        public static final String GREEN = "\033[0;32m";   // GREEN
-        public static final String YELLOW = "\033[0;33m";  // YELLOW
-        public static final String BLUE = "\033[0;34m";    // BLUE
-        public static final String PURPLE = "\033[0;35m";  // PURPLE
-        public static final String CYAN = "\033[0;36m";    // CYAN
-        public static final String WHITE = "\033[0;37m";   // WHITE
-
-        // Bold
-        public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
-        public static final String RED_BOLD = "\033[1;31m";    // RED
-        public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
-        public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-        public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
-        public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
-        public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-        public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
-
-        // Underline
-        public static final String BLACK_UNDERLINED = "\033[4;30m";  // BLACK
-        public static final String RED_UNDERLINED = "\033[4;31m";    // RED
-        public static final String GREEN_UNDERLINED = "\033[4;32m";  // GREEN
-        public static final String YELLOW_UNDERLINED = "\033[4;33m"; // YELLOW
-        public static final String BLUE_UNDERLINED = "\033[4;34m";   // BLUE
-        public static final String PURPLE_UNDERLINED = "\033[4;35m"; // PURPLE
-        public static final String CYAN_UNDERLINED = "\033[4;36m";   // CYAN
-        public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE
-
-        // Background
-        public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
-        public static final String RED_BACKGROUND = "\033[41m";    // RED
-        public static final String GREEN_BACKGROUND = "\033[42m";  // GREEN
-        public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
-        public static final String BLUE_BACKGROUND = "\033[44m";   // BLUE
-        public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
-        public static final String CYAN_BACKGROUND = "\033[46m";   // CYAN
-        public static final String WHITE_BACKGROUND = "\033[47m";  // WHITE
-
-        // High Intensity
-        public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
-        public static final String RED_BRIGHT = "\033[0;91m";    // RED
-        public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
-        public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
-        public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
-        public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
-        public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
-        public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
-
-        // Bold High Intensity
-        public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
-        public static final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
-        public static final String GREEN_BOLD_BRIGHT = "\033[1;92m"; // GREEN
-        public static final String YELLOW_BOLD_BRIGHT = "\033[1;93m";// YELLOW
-        public static final String BLUE_BOLD_BRIGHT = "\033[1;94m";  // BLUE
-        public static final String PURPLE_BOLD_BRIGHT = "\033[1;95m";// PURPLE
-        public static final String CYAN_BOLD_BRIGHT = "\033[1;96m";  // CYAN
-        public static final String WHITE_BOLD_BRIGHT = "\033[1;97m"; // WHITE
-
-        // High Intensity backgrounds
-        public static final String BLACK_BACKGROUND_BRIGHT = "\033[0;100m";// BLACK
-        public static final String RED_BACKGROUND_BRIGHT = "\033[0;101m";// RED
-        public static final String GREEN_BACKGROUND_BRIGHT = "\033[0;102m";// GREEN
-        public static final String YELLOW_BACKGROUND_BRIGHT = "\033[0;103m";// YELLOW
-        public static final String BLUE_BACKGROUND_BRIGHT = "\033[0;104m";// BLUE
-        public static final String PURPLE_BACKGROUND_BRIGHT = "\033[0;105m"; // PURPLE
-        public static final String CYAN_BACKGROUND_BRIGHT = "\033[0;106m";  // CYAN
-        public static final String WHITE_BACKGROUND_BRIGHT = "\033[0;107m";   // WHITE
-    }
-
 
     private static Scanner scanner = new Scanner(System.in);
 
@@ -99,8 +24,8 @@ public class App {
     private static void startApplication() {
 
         while (true) {
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n1. Login" + Colors.RESET);
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "2. Sign up" + Colors.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "\n1. Login" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "2. Sign up" + Color.RESET);
 
             String in = scanner.nextLine();
             int num;
@@ -127,10 +52,10 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         while(true) {
             System.out.println("\nLogin as: ");
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "1. Admin" + Colors.RESET);
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "2. Teacher" + Colors.RESET);
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "3. Student" + Colors.RESET);
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "0. back" + Colors.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "1. Admin" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "2. Teacher" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "3. Student" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "0. back" + Color.RESET);
 
             String in = scanner.nextLine();
             int userType;
@@ -144,7 +69,7 @@ public class App {
 
             if(userType == 0) break;
 
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "Enter your id: " + Colors.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "Enter your id: " + Color.RESET);
             String id = scanner.nextLine();
 
             UserFactory userFactory = new UserFactory();
@@ -160,10 +85,10 @@ public class App {
 
         loop1: while(true) {
             System.out.println("\nSign Up as: ");
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "1. Admin" + Colors.RESET);
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "2. Teacher" + Colors.RESET);
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "3. Student" + Colors.RESET);
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "0. back" + Colors.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "1. Admin" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "2. Teacher" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "3. Student" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "0. back" + Color.RESET);
 
             String in = scanner.nextLine();
             int userType;
@@ -203,13 +128,13 @@ public class App {
     }
 
     private static User createAdmin() {
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n1. Enter your Name" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n1. Enter your Name" + Color.RESET);
         String name = scanner.nextLine();
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n2. Enter your Email" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n2. Enter your Email" + Color.RESET);
         String email = scanner.nextLine();
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n3. Enter your Mobile Number" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n3. Enter your Mobile Number" + Color.RESET);
         String mobileNumber = scanner.nextLine();
 
         Admin admin = new Admin(name, email, mobileNumber);
@@ -220,13 +145,13 @@ public class App {
     }
 
     private static User createTeacher() {
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n1. Enter your Name" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n1. Enter your Name" + Color.RESET);
         String name = scanner.nextLine();
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n2. Enter your Email" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n2. Enter your Email" + Color.RESET);
         String email = scanner.nextLine();
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n3. Enter your Mobile Number" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n3. Enter your Mobile Number" + Color.RESET);
         String mobileNumber = scanner.nextLine();
 
         Teacher teacher = new Teacher(name, email, mobileNumber);
@@ -238,19 +163,19 @@ public class App {
 
     private static User createStudent() {
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n1. Enter your Name" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n1. Enter your Name" + Color.RESET);
         String name = scanner.nextLine();
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n2. Enter your Email" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n2. Enter your Email" + Color.RESET);
         String email = scanner.nextLine();
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n3. Enter your Mobile Number" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n3. Enter your Mobile Number" + Color.RESET);
         String mobileNumber = scanner.nextLine();
 
 
         int age;
         while (true) {
-            System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n4. Enter your Age" + Colors.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "\n4. Enter your Age" + Color.RESET);
             String ageIn = scanner.nextLine();
 
             try {
@@ -261,10 +186,10 @@ public class App {
             }
         }
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n5. Enter your Address" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n5. Enter your Address" + Color.RESET);
         String address = scanner.nextLine();
 
-        System.out.println(Colors.CYAN_BOLD_BRIGHT + "\n6. Enter your Gender" + Colors.RESET);
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\n6. Enter your Gender" + Color.RESET);
         String gender = scanner.nextLine();
 
         Student student = new Student(name, email, mobileNumber, age, address, gender);
--- src/main/java/ui/AdminUI.java
+++ src/main/java/ui/AdminUI.java
@@ -1,12 +1,149 @@
 package ui;
 
 import user.Admin;
+import user.Teacher;
 import user.User;
+import utils.Color;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class AdminUI implements UserUI {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private Admin admin;
+
     @Override
     public void start(User user) {
-        Admin admin = (Admin) user;
+        admin = (Admin) user;
         System.out.println("starting admin functionality");
+
+        loop1: while(true) {
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "\n1. Add Teacher" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "2. Remove Teacher" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "3. View All Teachers" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "4. View Teacher Details" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "5. Update Teacher Data" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "6. Add Student" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "7. Remove Student" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "8. View All Students" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "9. View Student Details" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "10. Update Student Data" + Color.RESET);
+            System.out.println(Color.CYAN_BOLD_BRIGHT + "0. Log out" + Color.RESET);
+
+            String in = scanner.nextLine();
+
+            switch (in) {
+                case "1":
+                    addTeacher();
+                    break;
+                case "2":
+                    removeTeacher();
+                    break;
+                case "3":
+                    viewAllTeachers();
+                    break;
+                case "4":
+                    viewTeacherDetails();
+                    break;
+                case "5":
+                    updateTeacherData();
+                    break;
+                case "6":
+                    addStudent();
+                    break;
+                case "7":
+                    removeStudent();
+                    break;
+                case "8":
+                    viewAllStudents();
+                    break;
+                case "9":
+                    viewStudentDetails();
+                    break;
+                case "10":
+                    updateStudentData();
+                    break;
+                case "0":
+                    break loop1;
+            }
+
+        }
+    }
+
+    private void addTeacher() {
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher Name" + Color.RESET);
+        String name = scanner.nextLine();
+
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher Email" + Color.RESET);
+        String email = scanner.nextLine();
+
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher Mobile Number" + Color.RESET);
+        String mobileNumber = scanner.nextLine();
+
+        admin.addTeacher(name, email, mobileNumber);
+    }
+
+    private void removeTeacher() {
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher ID" + Color.RESET);
+        String id = scanner.nextLine();
+
+        admin.removeTeacher(id);
+    }
+
+    private void viewAllTeachers() {
+        for (Teacher teacher: admin.viewAllTeachers()) {
+            System.out.println(teacher);
+        }
+    }
+
+    private void viewTeacherDetails() {
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher ID" + Color.RESET);
+        String id = scanner.nextLine();
+
+        System.out.println(admin.viewTeacherDetails(id));
+    }
+
+    private void updateTeacherData() {
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher ID" + Color.RESET);
+        String id = scanner.nextLine();
+
+        Teacher teacher = admin.viewTeacherDetails(id);
+        System.out.println(teacher);
+
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher Name (blank for no change)" + Color.RESET);
+        String in = scanner.nextLine();
+        if(!in.isEmpty()) teacher.setName(in);
+
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher Email (blank for no change)" + Color.RESET);
+        in = scanner.nextLine();
+        if(!in.isEmpty()) teacher.setEmail(in);
+
+        System.out.println(Color.CYAN_BOLD_BRIGHT + "\nEnter Teacher Mobile Number (blank for no change)" + Color.RESET);
+        in = scanner.nextLine();
+        if(!in.isEmpty()) teacher.setName(in);
+
+
+        admin.updateTeacherData(teacher);
+    }
+
+    private void addStudent() {
+
+    }
+
+    private void removeStudent() {
+
+    }
+
+    private void viewAllStudents() {
+
+    }
+
+    private void viewStudentDetails() {
+
+    }
+
+    private void updateStudentData() {
+
     }
 }
